d00534c03ff74b464f78838ab370920803bfcb49
gwanyong_04 <43106908+YYOngYYong@users.noreply.github.com>
2020-08-16 15:30:51 +0900

    coordinate : upgrade

--- coordinates/main.js
+++ coordinates/main.js
@@ -3,15 +3,18 @@ const vertical = document.querySelector(".vertical");
 const target = document.querySelector(".target");
 const tag = document.querySelector(".tag");
 
+const rect = target.getBoundingClientRect();
+const targetWidh = rect.width / 2;
+const targetHeight = rect.height / 2;
+
 document.addEventListener("mousemove", (event) => {
   const x = event.clientX;
   const y = event.clientY;
-  horizontal.style.top = `${y}px`;
-  vertical.style.left = `${x}px`;
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
-
+  horizontal.style.transform = `translateY(${y}px)`;
+  vertical.style.transform = `translateX(${x}px)`;
+  target.style.transform = `translate(${x - targetWidh}px,${
+    y - targetHeight
+  }px)`;
+  tag.style.transform = `translate(${x}px, ${y}px)`;
   tag.innerHTML = `${x},${y}`;
 });
